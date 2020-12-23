04fc9b21d33d876424f9c691220f2e6bb20981d7
miguel jimenez <denkatalabeno@gmail.com>
2020-12-24 01:41:50 +0800

    refactored common article functions to a utils file

--- app/javascript/controllers/article_1_controller.js
+++ app/javascript/controllers/article_1_controller.js
@@ -8,6 +8,7 @@
 // </div>
 
 import { Controller } from "stimulus"
+import { toggle } from './article_utils.js'
 
 export default class extends Controller {
   static targets = [ "main", "label" ]
@@ -18,45 +19,8 @@ export default class extends Controller {
     this.mainTarget.classList.add(this.wireframeClass);
   }
 
-  // Every article will have a common number of things to do:
-  // Toggle the wireframe class
-  // switch any images that are attached
-  // That's going to be the case for most categories
-  // make sense to have the same for all, and some special controllers for exceptions
-  // 1. try to clean this, so we can do all the actions with values and css, so we only need to
-  // store data in the html, and the code can be the same
-
-  // 2. Try to inherit, so we can have a generic controller for everyone
-  // StimulusController >  ArticlesController >  Article01Controller kind of thing
-  // packs Stimulus        has all common logic  has the name to create the connection
   toggle() {
-    if (this.active == false) {
-      this.active = true;
-      this.mainTarget.classList.remove('wireframe');
-      this.showAllImages();
-      this.labelTarget.innerText = "See Wireframe";
-    } else {
-      this.active = false;
-      this.mainTarget.classList.add('wireframe');
-      this.wireframeAllImages();
-      this.labelTarget.innerText = "See Live"
-    }
-  }
-
-  showAllImages() {
-  const allImages = this.selectAllImages();
-    allImages.forEach((img) => {
-      img.src = require(`images/${img.dataset.asset}-show.png`)}
-    )
-  }
-
-  wireframeAllImages() {
-    const allImages = this.selectAllImages();
-      allImages.forEach(img => img.src = require(`images/${img.dataset.asset}-wireframe.png`))
-  }
-
-  selectAllImages() {
-    return this.mainTarget.querySelectorAll('img');
+    toggle(this);
   }
 
 }
---
+++ app/javascript/controllers/article_utils.js
@@ -0,0 +1,31 @@
+  const toggle = (controller) => {
+    if (controller.active == false) {
+      controller.active = true;
+      controller.mainTarget.classList.remove('wireframe');
+      showAllImages(controller);
+      controller.labelTarget.innerText = "See Wireframe";
+    } else {
+      controller.active = false;
+      controller.mainTarget.classList.add('wireframe');
+      wireframeAllImages(controller);
+      controller.labelTarget.innerText = "See Live"
+    }
+  }
+
+  const showAllImages = (controller) => {
+  const allImages = selectAllImages(controller);
+    allImages.forEach((img) => {
+      img.src = require(`images/${img.dataset.asset}-show.png`)}
+    )
+  }
+
+  const wireframeAllImages = (controller) => {
+    const allImages = selectAllImages(controller);
+      allImages.forEach(img => img.src = require(`images/${img.dataset.asset}-wireframe.png`))
+  }
+
+  const selectAllImages = (controller) => {
+    return controller.mainTarget.querySelectorAll('img');
+  }
+
+  export { toggle }
