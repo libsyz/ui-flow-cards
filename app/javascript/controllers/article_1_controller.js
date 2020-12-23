e7c9f48b3516bb60a1616e9903a4884ff4b3f14d
miguel jimenez <denkatalabeno@gmail.com>
2020-12-24 00:35:55 +0800

    using css values now for declaring the wireframe class

--- app/javascript/controllers/article_1_controller.js
+++ app/javascript/controllers/article_1_controller.js
@@ -11,12 +11,24 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "main" ]
+  static classes = [ "wireframe" ]
   active = false;
 
   connect() {
-    console.log("I am connected!");
+    this.mainTarget.classList.add(this.wireframeClass);
   }
 
+  // Every article will have a common number of things to do:
+  // Toggle the wireframe class
+  // switch any images that are attached
+  // That's going to be the case for most categories
+  // make sense to have the same for all, and some special controllers for exceptions
+  // 1. try to clean this, so we can do all the actions with values and css, so we only need to
+  // store data in the html, and the code can be the same
+
+  // 2. Try to inherit, so we can have a generic controller for everyone
+  // StimulusController >  ArticlesController >  Article01Controller kind of thing
+  // packs Stimulus        has all common logic  has the name to create the connection
   toggle() {
     if (this.active == false) {
       this.active = true;
