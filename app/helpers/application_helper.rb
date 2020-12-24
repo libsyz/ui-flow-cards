6a3e92e301966ccc5bf0077eee002864d2efa443
miguel jimenez <denkatalabeno@gmail.com>
2020-12-25 00:08:52 +0800

    refactored section component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_checkbox(section_name, number)
+    raw("<input type=\"checkbox\"
+         class=\"custom-control-input\"
+         id=\"#{section_name}Layout#{number}\"
+         data-action=\"change->#{section_name}-#{number}#toggle\">")
+  end
+
+  def render_label(section_name, number)
+    raw("<label class=\"custom-control-label text-secondary\"
+                for=\"#{section_name}Layout#{number}\"
+                data-#{section_name}-#{number}-target=\"label\">See Live</label>"
+      )
+  end
 end
