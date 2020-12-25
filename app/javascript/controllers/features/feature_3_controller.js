9e3af54ba701f390069abdc7269b868b31d14969
miguel jimenez <denkatalabeno@gmail.com>
2020-12-25 22:42:18 +0800

    features features

--- app/javascript/controllers/features/feature_3_controller.js
+++ app/javascript/controllers/features/feature_3_controller.js
@@ -11,7 +11,7 @@ import { Controller } from "stimulus"
 import { toggle } from './../articles/article_utils.js'
 
 export default class extends Controller {
-  static targets = [ "main", "label" ]
+  static targets = [ "main", "label", "screen" ]
   static classes = [ "wireframe" ]
   active = false;
 
@@ -21,6 +21,17 @@ export default class extends Controller {
 
   toggle() {
     toggle(this);
+    this.toggleScreen();
+  }
+
+  toggleScreen() {
+    if (this.screenTarget.classList.contains('bg-primary')) {
+      this.screenTarget.classList.remove('bg-primary');
+      this.screenTarget.classList.add('bg-fancy-mobile')
+    } else {
+      this.screenTarget.classList.add('bg-primary');
+      this.screenTarget.classList.remove('bg-fancy-mobile')
+    }
   }
 
 }
