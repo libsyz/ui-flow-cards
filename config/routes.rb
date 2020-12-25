46301020e5e596d62899446de143836d750c7b87
miguel jimenez <denkatalabeno@gmail.com>
2020-12-25 16:04:09 +0800

    feature 1 layout working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
+  get '/articles', to: 'articles#index'
+  get '/features', to: 'features#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
