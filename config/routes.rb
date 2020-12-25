9b147d868100975255605be8cc5b471b831db0ef
miguel jimenez <denkatalabeno@gmail.com>
2020-12-25 22:49:14 +0800

    starting forms section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'forms/index'
   root to: 'articles#index'
   get '/articles', to: 'articles#index'
   get '/features', to: 'features#index'
+  get '/forms', to: 'forms#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
