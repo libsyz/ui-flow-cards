6fc202cd6c299203504f4ad451b19e552bb80e9e
miguel jimenez <denkatalabeno@gmail.com>
2020-12-25 12:38:25 +0800

    namespaced controllers on stimulus, changed html accordingly to reflect the changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,13 @@ module ApplicationHelper
     raw("<input type=\"checkbox\"
          class=\"custom-control-input\"
          id=\"#{section_name}Layout#{number}\"
-         data-action=\"change->#{section_name}-#{number}#toggle\">")
+         data-action=\"change->#{section_name.pluralize}--#{section_name}-#{number}#toggle\">")
   end
 
   def render_label(section_name, number)
     raw("<label class=\"custom-control-label text-secondary\"
                 for=\"#{section_name}Layout#{number}\"
-                data-#{section_name}-#{number}-target=\"label\">See Live</label>"
+                data-#{section_name.pluralize}--#{section_name}-#{number}-target=\"label\">See Live</label>"
       )
   end
 end
