63f71702598ce8b486a56564abb264b18995b8c1
miguel jimenez <denkatalabeno@gmail.com>
2020-12-26 23:45:33 +0800

    map now working

--- app/javascript/controllers/maps/map_2_controller.js
+++ app/javascript/controllers/maps/map_2_controller.js
@@ -1,12 +1,3 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus";
 import mapboxgl from 'mapbox-gl';
 import { toggle } from './map_utils.js';
@@ -15,21 +6,27 @@ export default class extends Controller {
   static targets = [ "main", "label", "map", "mapWireframe" ]
   static classes = [ "wireframe" ]
   active = false;
+  mapDrawn = false;
 
   connect() {
     this.mainTarget.classList.add(this.wireframeClass);
-    this.drawMap();
+    this.toggleIcons();
+
   }
 
   toggle() {
     toggle(this);
+    this.toggleIcons();
+    if (this.mapDrawn == false) {
+      this.drawMap();
+    }
   }
 
   drawMap() {
     mapboxgl.accessToken = 'pk.eyJ1IjoibGlic3l6IiwiYSI6ImNrajVwcHBqNTA1bzcyeXA1eGQwb2pxdGcifQ.asgTo8KQ1d8SrnT_y9LrFQ'
 
     const map = new mapboxgl.Map({
-      container: 'map',
+      container: 'map-2',
       style: 'mapbox://styles/mapbox/streets-v9',
       center: [ 103.830802, 1.305694 ],
       zoom: 12
@@ -38,6 +35,19 @@ export default class extends Controller {
     new mapboxgl.Marker()
       .setLngLat([ 103.830802, 1.305694 ])
       .addTo(map);
+
+    this.mapDrawn = true;
+  }
+
+  toggleIcons() {
+    const icons = this.mainTarget.querySelectorAll('.fas');
+    icons.forEach((icon) => {
+      if (this.active == true) {
+        icon.classList.remove('d-none');
+      } else {
+        icon.classList.add('d-none');
+      }
+    })
   }
 
 }
