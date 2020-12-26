3926da19438ef24ed0b86162d34c6d8bb37d03d3
miguel jimenez <denkatalabeno@gmail.com>
2020-12-26 20:31:06 +0800

    added mapbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'maps/index'
   get 'forms/index'
   root to: 'articles#index'
   get '/articles', to: 'articles#index'
   get '/features', to: 'features#index'
   get '/forms', to: 'forms#index'
+  get '/maps', to: 'maps#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
