0152d4dfaedd980bc23cea23d6862bcf944c44c3
miguel jimenez <denkatalabeno@gmail.com>
2020-12-28 00:11:40 +0800

    implemented 3rd map view

---
+++ app/javascript/controllers/maps/map_3_controller.js
@@ -0,0 +1,48 @@
+import { Controller } from "stimulus";
+import mapboxgl from 'mapbox-gl';
+import { toggle } from './map_utils.js';
+
+export default class extends Controller {
+  static targets = [ "main", "label", "map", "mapWireframe" ]
+  static classes = [ "wireframe" ]
+  active = false;
+  mapDrawn = false;
+
+  connect() {
+    this.mainTarget.classList.add(this.wireframeClass);
+  }
+
+  toggle() {
+    toggle(this);
+    if (this.mapDrawn == false) {
+      this.drawMap();
+    }
+  }
+
+  drawMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGlic3l6IiwiYSI6ImNrajVwcHBqNTA1bzcyeXA1eGQwb2pxdGcifQ.asgTo8KQ1d8SrnT_y9LrFQ'
+    const map = new mapboxgl.Map({
+      container: 'map-3',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [ 103.830802, 1.305694 ],
+      zoom: 12
+     });
+
+    new mapboxgl.Marker()
+      .setLngLat([ 103.830802, 1.305694 ])
+      .addTo(map);
+
+    this.mapDrawn = true;
+  }
+
+  toggleIcons() {
+    const icons = this.mainTarget.querySelectorAll('.fas');
+    icons.forEach((icon) => {
+      if (this.active == true) {
+        icon.classList.remove('d-none');
+      } else {
+        icon.classList.add('d-none');
+      }
+    })
+  }
+}
