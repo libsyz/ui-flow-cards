a705cee93579f84344d4ed552dbc7e171dc9a1bd
miguel jimenez <denkatalabeno@gmail.com>
2020-12-29 23:07:09 +0800

    started team views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'teams/index'
   get 'ecommerces/index'
   get 'maps/index'
   get 'forms/index'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   get '/forms', to: 'forms#index'
   get '/maps', to: 'maps#index'
   get '/ecommerce', to: 'ecommerces#index'
+  get '/team', to: 'teams#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
