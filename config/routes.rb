0249dfb47413dc404bafdb0efb41feaecbd2a23e
miguel jimenez <denkatalabeno@gmail.com>
2020-12-29 23:55:28 +0800

    first layout for teams complete

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get '/forms', to: 'forms#index'
   get '/maps', to: 'maps#index'
   get '/ecommerce', to: 'ecommerces#index'
-  get '/team', to: 'teams#index'
+  get '/teams', to: 'teams#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
