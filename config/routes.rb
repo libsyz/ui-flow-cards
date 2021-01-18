41e88afc6bc81bcaeff4ac48ae50b9b24a1197ee
miguel jimenez <denkatalabeno@gmail.com>
2021-01-18 21:38:44 +0800

    added section for login layouts

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get '/dashboards', to: 'dashboards#index'
   get '/searches', to: 'searches#index'
   get '/testimonials', to: 'testimonials#index'
+  get '/logins', to: 'logins#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
